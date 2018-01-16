fb675185ee74e6dcee5632790aa24abdffa30c8e
revannth.github.io <revannth1997@gmail.com>
2018-01-16 18:02:45 +0530

    Reinitialised

--- ccd_snr/paste.py
+++
@@ -1,8 +0,0 @@
-import os
-f = open('names.txt', 'w')
-
-for root, dirs, files in os.walk('./'):
-	for newname in files:
-		filename = os.path.join(newname)
-		newstring = "ccd_snr/" + filename + '\n'
-		f.write(newstring)
